ce2b17e8982b342facdedd094915d47142f4e131
rzapaja <rzapaja@gmail.com>
2024-11-26 19:22:50 -0800

    home PC: finished exercise 4 + 6

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr,...args) {
+
+    const newArray = [];
+
+    arr.forEach((item) => {
+        if (!args.includes(item)){
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
 
+    let small;
+    let big;
+
+    if (num1 < num2){
+        small = num1;
+        big = num2;
+    } else if (num1 > num2){
+        small = num2;
+        big = num1;
+    } else {
+        // for when num1 = num2
+    };
+
+    let counter = small;
+    let sum = small;
+    
+    while (counter < big){
+        ++counter;
+        sum += counter;
+    }
+
+    
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }else if (!(typeof num1 === 'number' || typeof num2 === 'number')){
+        return "ERROR";
+    } else{
+        return sum;
+    }
 };
 
 // Do not edit below this line
